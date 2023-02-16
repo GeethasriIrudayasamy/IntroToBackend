0c2e41ab206d1621079ad283d04f818ba8210b00
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2023-02-16 21:15:09 +0530

    knowing about request and response

--- firstfile.js
+++ firstfile.js
@@ -1,7 +1,38 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("Geethasri I");
+    if (req.url === "/") {
+        res.setHeader("content-type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>A new App</title></head>");
+        res.write("<body><h3>Welcome to my Node Js project</h3></body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (req.url === "/home") {
+        res.setHeader("content-type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>A new App</title></head>");
+        res.write("<body><h3>Welcome home</h3></body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (req.url === "/about") {
+        res.setHeader("content-type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>A new App</title></head>");
+        res.write("<body><h3>Welcome to About Us page</h3></body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (req.url === "/node") {
+        res.setHeader("content-type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>A new App</title></head>");
+        res.write("<body><h3>Welcome to my Node Js project</h3></body>");
+        res.write("</html>");
+        return res.end();
+    }
 });
 
 server.listen(4000);
